217529c8c1c4d1986c7880fd38904e1aa33c045c
Wojciech Kleszcz <wkleszcz@student.agh.edu.pl>
2025-04-13 14:01:31 +0200

    Added environment variables management

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from ipum-lab1!")
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    load_dotenv(".env." + environment)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
---
+++ settings.py
@@ -0,0 +1,16 @@
+# settings.py
+from pydantic_settings import BaseSettings
+from pydantic import field_validator, ValidationError
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @field_validator("ENVIRONMENT")
+    @classmethod
+    def validate_environment(cls, v):
+        # prepare validator that will check whether the value of ENVIRONMENT is in (dev, test, prod)
+        if v not in ("dev", "test", "prod"):
+            raise ValidationError("ENVIRONMENT must be one of: dev, test, prod")
+        return v
